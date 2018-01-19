e471ea2c509149eba0b7615dc9411a1e143628b8
Tito Yslas <roberto.yslas@gmail.com>
2018-01-18 19:00:33 -0700

    added routes to Posts and NewPost!

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 // import axios from '../../axios';
+import { Route } from 'react-router-dom';
+
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -13,11 +16,13 @@ class Blog extends Component {
             <nav>
               <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">New Post</a></li>
+                <li><a href="/new-post">New Post</a></li>
               </ul>
             </nav>
           </header>
-          <Posts />
+          {/* <Route path="/" exact render={() => <Posts />} /> */}
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
         </div>
     );
   }
