c9f54431f3bdccabec21efa9a9f372e8c93ecd80
Tito Yslas <roberto.yslas@gmail.com>
2018-01-18 19:11:50 -0700

    added the Link component to prevent the default reload behavior of routing. this prevents the page from reloading when routing to a different view of the SPA and for the application to retain its state

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 // import axios from '../../axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -15,8 +15,12 @@ class Blog extends Component {
           <header>
             <nav>
               <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/new-post">New Post</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to={{
+                  pathname: '/new-post',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }}>New Post</Link></li>
               </ul>
             </nav>
           </header>
