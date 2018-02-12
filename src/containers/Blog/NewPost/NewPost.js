00e49c4bb8e637153278be50601f104a0bcbe56d
Tito Yslas <roberto.yslas@gmail.com>
2018-02-12 16:53:33 -0700

    learning how to use routing-related props

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -22,6 +22,10 @@ class NewPost extends Component {
       });
   }
 
+  componentDidMount() {
+    console.log(this.props);
+  }
+
   render () {
     return (
       <div className="NewPost">
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -10,6 +10,7 @@ class Posts extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props);
     axios.get('/posts')
       .then(res => {
         const posts = res.data.slice(0, 4)
